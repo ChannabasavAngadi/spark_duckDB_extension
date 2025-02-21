26ab3779062898c7a4a84922ac76252c18802cce
Channabasav Angadi <96965059+ChannabasavAngadi@users.noreply.github.com>
2025-02-21 08:11:34 +0530

    Update example_usage.py

--- duckdb_extension/example_usage.py
+++ duckdb_extension/example_usage.py
@@ -6,7 +6,7 @@ spark = SparkSession.builder.appName("DuckDB Example").getOrCreate()
 # Register the DuckDB extension
 register_duckdb_extension(spark)
 
-df=spark.read.csv("DME_LIST.csv",header=True)
+df=spark.read.csv("employees.csv",header=True)
 
 # Use the custom extension to write the DataFrame to DuckDB and specify the table name
-df.write.duckdb_extension(database="./my_db.duckdb", table_name="newtable", mode="append")
+df.write.duckdb_extension(database="./company_db.duckdb", table_name="employees", mode="append")
